731466c2c924782ffa8f2c8ea6421c8aa3e35bf4
Oghenemarho Orukele <morukele@yahoo.com>
2021-11-01 18:43:52 +0100

    finished phonebook-app backend

--- models/person.js
+++ models/person.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connectiong to MongoDB:", error.message);
-  });
+    console.log('error connectiong to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,16 +25,16 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,48 +1,48 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
 if (process.argv.length >= 3) {
-  const password = process.argv[2];
-  const _name = process.argv[3];
-  const _number = process.argv[4];
+  const password = process.argv[2]
+  const _name = process.argv[3]
+  const _number = process.argv[4]
 
-  const url = `mongodb+srv://dbMaster:${password}@cluster0.nori7.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+  const url = `mongodb+srv://dbMaster:${password}@cluster0.nori7.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
-  mongoose.connect(url);
+  mongoose.connect(url)
 
   const personSchema = new mongoose.Schema({
     name: String,
     number: String,
-  });
+  })
 
-  const Person = mongoose.model("Person", personSchema);
+  const Person = mongoose.model('Person', personSchema)
 
   if (process.argv.length > 3) {
     const person = new Person({
       name: _name,
       number: _number,
-    });
+    })
 
     person.save().then((result) => {
-      console.log(`added ${result.name} number ${result.number} to phonebook`);
-      mongoose.connection.close();
-    });
+      console.log(`added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
   } else if (process.argv.length === 3) {
     Person.find({}).then((result) => {
-      console.log("phonebook:");
+      console.log('phonebook:')
       result.forEach((person) => {
-        console.log(`${person.name} ${person.number}`);
-      });
-      mongoose.connection.close();
-    });
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
   } else {
-    console.log("Name and phonenumber are required fields");
-    process.exit(1);
+    console.log('Name and phonenumber are required fields')
+    process.exit(1)
   }
 }
